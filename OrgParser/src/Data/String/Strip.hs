5833380ec3dd6e50f7a57e80d5c1310a16a00e72
jumpei <junnpit@gmail.com>
2025-03-14 20:20:39 +0900

    defining proceed.

--- OrgParser/src/Data/String/Strip.hs
+++ OrgParser/src/Data/String/Strip.hs
@@ -2,38 +2,85 @@ module Data.String.Strip
   (strip
   , orgTagsParse
   , orgTimeParse
-  , orgDateParse
+  , orgDateYMDParse
+  , orgDateCoreParse
+  , orgTitleParse
+  , timeToDiffTime
   )  where
 
 import Data.Char
+import Data.Time
 import Text.Parsec
 import Control.Monad
 
 type Parser = Parsec String ()
 
+---tags---------------------------------------------------------------------------------------------
 orgTagsParse :: Parser [String]
 orgTagsParse =
-  char ':' *> ( mainParse <|> return [])
+  char ':' *> (mainParse <|> return [])
   where
     inner     = many (noneOf " :\t\n") <* lookAhead (char ':')
     mainParse = (:) <$> inner <*> orgTagsParse
+---tags---------------------------------------------------------------------------------------------
 
+---time---------------------------------------------------------------------------------------------
 orgTimeParse :: Parser (Int, Int)
 orgTimeParse = do
-  hs <- read <$> (count 2 digit <* char ':')
-  ms <- read <$> count 2 digit
-  guard $ (0 <= hs && hs <= 24) && (0 <= ms && ms <= 59)
-  return (hs, ms)
+  [h, m] <- map read <$> sequence [ count 2 digit <* char ':'
+                                  , count 2 digit]
+  guard $ h `rangep` (0, 24) && m `rangep` (0, 59)
+  return (h, m)
 
-orgDateParse :: Parser (Int, Int, Int)
-orgDateParse = do
-  y <- read <$> (count 4 digit <* char '-')
-  m <- read <$> (count 2 digit <* char '-')
-  d <- read <$> count 2 digit
-  guard $ (2024 <= y && y <= 2099)
-  guard $ (1 <= m && m <= 12)
-  guard $ (1 <= d && d <= 31)
+orgDateYMDParse :: Parser (Int, Int, Int)
+orgDateYMDParse = do
+  [y, m, d] <- map read <$> sequence [ count 4 digit <* char '-'
+                                     , count 2 digit <* char '-'
+                                     , count 2 digit]
+  guard $
+    y `rangep` (2024, 2099) && m `rangep` (1, 12) && d `rangep` (1, 31)
   return (y, m, d)
+
+orgDateCoreParse :: Parser UTCTime
+orgDateCoreParse = do
+  (y, m, d) <- orgDateYMDParse <* space
+  (h, mi)   <-
+    -- ((oneOf "月火水木金土日") >> space >> orgTimeParse) <|> return (0,0)
+    try (anyChar >> space >> orgTimeParse) <|> return (0,0)
+  return $ UTCTime (fromGregorian (toInteger y) m d) $ timeToDiffTime (h, mi)
+
+orgActiveDateParse, orgInactiveDateParse :: Parser UTCTime
+orgActiveDateParse   = between (char '<') (char '>') orgDateCoreParse
+orgInactiveDateParse = between (char '[') (char ']') orgDateCoreParse
+-- SCHEDULED:
+-- DEADLINE:
+-- CLOSED:
+rangep :: Int -> (Int, Int) -> Bool
+rangep target (low, high)
+  | low <= target && target <= high = True
+  | otherwise                       = False
+
+timeToDiffTime :: (Int, Int) -> DiffTime
+timeToDiffTime (h, m) =
+  secondsToDiffTime $ (toInteger h) * 3600 + (toInteger m) * 60
+---time---------------------------------------------------------------------------------------------
+
+---title--------------------------------------------------------------------------------------------
+orgTitleParse :: Parser String
+orgTitleParse = do
+  stars <- many (char '*') <* many1 space
+  todo  <- try (todokwds <* many1 space) <|> return mempty
+  _     <- try (indicate >> many1 space) <|> return mempty
+  title <- many anyChar
+  return title
+  where
+    todokwds = choice $ map string ["TODO", "DONE", "WAIT", "PEND"]
+    indicate = char '[' >> many digit >>
+               char '/' >> many digit >> char ']' >> return ()
+---title--------------------------------------------------------------------------------------------
+
+---property-----------------------------------------------------------------------------------------
+---property-----------------------------------------------------------------------------------------
 
 strip :: String -> String
 strip = dropWhile isSpace . reverse . dropWhile isSpace . reverse
--- OrgParser/test/Data/String/StripSpec.hs
+++ OrgParser/test/Data/String/StripSpec.hs
@@ -5,6 +5,7 @@ import Test.QuickCheck
 import Text.Parsec
 
 import Data.String.Strip
+import Data.Time
 
 -- `main` is here so that this module can be run from GHCi on its own.  It is
 -- not needed for automatic spec discovery.
@@ -16,10 +17,27 @@ spec = do
   describe "strip" $ do
     it "orgTagsParse" $ do
       parse orgTagsParse "" ":hoge:foo:" `shouldBe` Right ["hoge", "foo"]
+      -- parse orgTagsParse "" ":hoge:foo"  `shouldBe` Left ""
     it "orgTimeParse" $ do
       parse orgTimeParse "" "12:00" `shouldBe` Right (12, 0)
-    it "orgDateParse" $ do
-      parse orgDateParse "" "2024-12-01" `shouldBe` Right (2024, 12, 1)
+    it "orgDateYMDParse" $ do
+      parse orgDateYMDParse "" "2024-12-01" `shouldBe` Right (2024, 12, 1)
+    it "orgDateCoreParse" $ do
+      parse orgDateCoreParse "" "2024-12-01 月 12:00" `shouldBe`
+        Right (UTCTime (fromGregorian 2024 12 1) (timeToDiffTime (12, 0)))
+    it "orgDateCoreParse2" $ do
+      parse orgDateCoreParse "" "2024-12-01 月" `shouldBe`
+        Right (UTCTime (fromGregorian 2024 12 1) (timeToDiffTime (0, 0)))
+    it "orgTitleParse" $ do
+      parse orgTitleParse "" "**** hoge" `shouldBe` Right "hoge"
+    it "orgTitleParse2" $ do
+      parse orgTitleParse "" "**** [10/10] hoge" `shouldBe` Right "hoge"
+    it "orgTitleParse3" $ do
+      parse orgTitleParse "" "**** TODO [10/10] hoge" `shouldBe` Right "hoge"
+    it "orgTitleParse4" $ do
+      parse orgTitleParse "" "**** DONE [10/10] hoge" `shouldBe` Right "hoge"
+    it "orgTitleParse5" $ do
+      parse orgTitleParse "" "*** DONE [10/10] ho ge" `shouldBe` Right "ho ge"
     -- it "removes leading and trailing whitespace" $ do
     --   strip "\t  foo bar\n" `shouldBe` "foo bar"
     -- it "is idempotent" $ property $
