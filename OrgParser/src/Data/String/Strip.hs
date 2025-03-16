b5a1cffe2ceae977eb78b55add8a6058bfdd83e7
jumpei <junnpit@gmail.com>
2025-03-16 12:51:50 +0900

    updated.

--- OrgParser/src/Data/String/Strip.hs
+++ OrgParser/src/Data/String/Strip.hs
@@ -4,27 +4,116 @@ module Data.String.Strip
   , orgTimeParse
   , orgDateYMDParse
   , orgDateCoreParse
+  , orgDateParse
   , orgTitleParse
+  , orgPropertyParse
+  , orgLinkParse
   , timeToDiffTime
+  , OrgElement (..)
   )  where
 
 import Data.Char
 import Data.Time
+import Data.Either
 import Text.Parsec
 import Control.Monad
+import System.IO
+
+-- class OrgElement a where
+--   toString :: a -> String
+
+-- instance OrgElement OrgTimeStamp where
+--   toString (Active u)     = show u
+--   toString (InActive u)   = show u
+--   toString (Scheduled u)  = show u
+--   toString (Deadline u)   = show u
+--   toString (Closed u)     = show u
+--   toString (Range (s, e)) = show s ++ "-" ++ show e
+
+-- instance OrgElement OrgTitle where
+--   toString o = title o
+
+-- instance OrgElement OrgProperty where
+--   toString PropBegin          = "<"
+--   toString PropEnd            = ">"
+--   toString (PropLocation s)   = s
+--   toString (PropJoiner s)     = s
+--   toString (PropOther (k, v)) = k ++ ": " ++ v
+
+-- instance OrgElement OrgLine where
+--   toString (OrgLine s) = s
+
+-- instance OrgElement OrgOther where
+--   toString (OrgOther s) = s
 
 type Parser = Parsec String ()
+type Destination = String
+type Explanation = String
 
----tags---------------------------------------------------------------------------------------------
+data TimeStamp
+data Title
+data Property
+data Link
+
+data OrgElement a = OrgTitle { title :: String
+                             , level :: Int
+                             , todo  :: Maybe String
+                             , children :: [OrgElement Title]}
+                  | Active UTCTime
+                  | InActive UTCTime
+                  | Scheduled UTCTime
+                  | Deadline UTCTime
+                  | Closed UTCTime
+                  | Range (UTCTime, UTCTime)
+
+                  | PropBegin
+                  | PropEnd
+                  | PropLocation String
+                  | PropJoiner String
+                  | PropOther (String, String)
+
+                  | OrgLink Destination (Maybe Explanation)
+                  | OrgOther String
+                  deriving (Show, Eq)
+
+-- data OrgTimeStamp = Active UTCTime
+--                   | InActive UTCTime
+--                   | Scheduled UTCTime
+--                   | Deadline UTCTime
+--                   | Closed UTCTime
+--                   | Range (UTCTime, UTCTime)
+--                   deriving (Show, Eq)
+
+-- data OrgTitle = OrgTitle { title    :: String
+--                          , level    :: Int
+--                          , todo     :: Maybe String
+--                          , children :: [OrgTitle]
+--                          }
+--               deriving (Show, Eq)
+
+-- data OrgProperty = PropBegin
+--                  | PropEnd
+--                  | PropLocation String
+--                  | PropJoiner String
+--                  | PropOther (String, String)
+--                  deriving (Show, Eq)
+
+-- newtype OrgLine = OrgLine String deriving (Show, Eq)
+
+-- data OrgLink = OrgLink Destination (Maybe Explanation) deriving (Show, Eq)
+
+-- newtype OrgOther = OrgOther String deriving (Show, Eq)
+
+---tags-----------------------------------------------------
 orgTagsParse :: Parser [String]
 orgTagsParse =
   char ':' *> (mainParse <|> return [])
   where
-    inner     = many (noneOf " :\t\n") <* lookAhead (char ':')
+    inner     = many1 (noneOf " :\t\n") <* lookAhead (char ':')
     mainParse = (:) <$> inner <*> orgTagsParse
----tags---------------------------------------------------------------------------------------------
+---tags-----------------------------------------------------
 
----time---------------------------------------------------------------------------------------------
+---time-----------------------------------------------------
 orgTimeParse :: Parser (Int, Int)
 orgTimeParse = do
   [h, m] <- map read <$> sequence [ count 2 digit <* char ':'
@@ -43,18 +132,31 @@ orgDateYMDParse = do
 
 orgDateCoreParse :: Parser UTCTime
 orgDateCoreParse = do
-  (y, m, d) <- orgDateYMDParse <* space
+  (y, m, d) <- orgDateYMDParse <* space <* anyChar
   (h, mi)   <-
     -- ((oneOf "月火水木金土日") >> space >> orgTimeParse) <|> return (0,0)
-    try (anyChar >> space >> orgTimeParse) <|> return (0,0)
+    try (space >> orgTimeParse) <|> return (0,0)
   return $ UTCTime (fromGregorian (toInteger y) m d) $ timeToDiffTime (h, mi)
 
-orgActiveDateParse, orgInactiveDateParse :: Parser UTCTime
-orgActiveDateParse   = between (char '<') (char '>') orgDateCoreParse
-orgInactiveDateParse = between (char '[') (char ']') orgDateCoreParse
--- SCHEDULED:
--- DEADLINE:
--- CLOSED:
+orgDateParse :: Parser (OrgElement TimeStamp)
+orgDateParse = do
+  try range
+    <|> try active
+    <|> try inactive
+    <|> try schedule
+    <|> try deadline
+    <|> try closed
+  where
+    acore    = between (char '<') (char '>') orgDateCoreParse
+    icore    = between (char '[') (char ']') orgDateCoreParse
+    rangesep = many space >> char '-' >> many space
+    active   =    Active <$> acore
+    inactive =  InActive <$> icore
+    schedule = Scheduled <$> (string "SCHEDULED: " *> acore)
+    deadline =  Deadline <$> (string "DEADLINE: "  *> acore)
+    closed   =    Closed <$> (string "CLOSED: "    *> icore)
+    range    = Range <$> ((,) <$> (acore <* rangesep) <*> acore)
+
 rangep :: Int -> (Int, Int) -> Bool
 rangep target (low, high)
   | low <= target && target <= high = True
@@ -63,24 +165,89 @@ rangep target (low, high)
 timeToDiffTime :: (Int, Int) -> DiffTime
 timeToDiffTime (h, m) =
   secondsToDiffTime $ (toInteger h) * 3600 + (toInteger m) * 60
----time---------------------------------------------------------------------------------------------
+---time-----------------------------------------------------
 
----title--------------------------------------------------------------------------------------------
-orgTitleParse :: Parser String
+---title----------------------------------------------------
+orgTitleParse :: Parser (OrgElement Title)
 orgTitleParse = do
-  stars <- many (char '*') <* many1 space
-  todo  <- try (todokwds <* many1 space) <|> return mempty
-  _     <- try (indicate >> many1 space) <|> return mempty
-  title <- many anyChar
-  return title
+  stars  <- many1 (char '*') <* many1 space
+  todo'  <- try (Just <$> todokwds <* many1 space) <|> return Nothing
+  _      <- try (indicate >> many1 space) <|> return mempty
+  title' <- coreP
+  return $ OrgTitle { title    = title'
+                    , level    = length stars
+                    , todo     = todo'
+                    , children = [] }
   where
     todokwds = choice $ map string ["TODO", "DONE", "WAIT", "PEND"]
     indicate = char '[' >> many digit >>
                char '/' >> many digit >> char ']' >> return ()
----title--------------------------------------------------------------------------------------------
+    coreF    = manyTill' anyToken
+    coreP    = do
+      try (coreF (orgDateParse >> many space >> orgTagsParse))
+      <|> try (coreF orgDateParse)
+      <|> try (coreF orgTagsParse)
+      <|> many anyChar
+---title----------------------------------------------------
 
----property-----------------------------------------------------------------------------------------
----property-----------------------------------------------------------------------------------------
+---property-------------------------------------------------
+orgPropertyParse :: Parser (OrgElement Property)
+orgPropertyParse = do
+  try pbegin <|> try pend <|> try location <|> try joiner
+    <|> PropOther <$> ((,) <$> otherPname <*> otherPval)
+  where
+    pbegin     = string ":PROPERTIES:" >> return PropBegin
+    pend       = string ":END:"  >> return PropEnd
+    pstr       = (:) <$> (satisfy (/= ':')) <*> many anyChar
+    location   = PropLocation <$> (string ":LOCATION:" >> many space >> pstr)
+    joiner     = PropJoiner <$> (string ":JOINER:" >> many space >> pstr)
+    otherPname = char ':' *> many1 (noneOf ":\n") <* char ':'
+    otherPval  = many space >> ((:) <$> (satisfy (/= ' ')) <*> many anyToken)
+---property-------------------------------------------------
+
+---link-----------------------------------------------------
+orgLinkParse :: Parser (OrgElement Link)
+orgLinkParse = do
+  try link1 <|> try link2
+  where
+    link1 = do
+      link <- string "[[" *> many1 (satisfy (/= ']')) <* string "]]"
+      return (OrgLink link Nothing)
+    link2 = do
+      link <- string "[[" *> manyTill (satisfy (/= ']')) (string "]")
+      expl <- string "[" *> manyTill (satisfy (/= ']')) (string "]")
+      return (OrgLink link (Just expl))
+---link-----------------------------------------------------
+
+---line-----------------------------------------------------
+orgLineParse :: String -> Either ParseError (OrgElement Title)
+orgLineParse = parse orgTitleParse ""
+
+-- orgLineCoreParse :: Parser [OrgElement a]
+orgLineCoreParse = do
+  try eof >> return []
+  <|> try ((:) <$> orgTitleParse <*> orgLineCoreParse)
+--   <|> try ((:) <$> orgTitleParse    <*> orgLineCoreParse)
+  <|> try ((:) <$> orgDateParse     <*> orgLineCoreParse)
+  -- <|> try ((:) <$> orgLinkParse     <*> orgLineCoreParse)
+  -- <|> try ((:) <$> orgPropertyParse <*> orgLineCoreParse)
+  -- <|> try ((:) <$> other            <*> orgLineCoreParse)
+  -- where
+  --   other = OrgOther <$> many1 anyChar
+---line-----------------------------------------------------
 
 strip :: String -> String
 strip = dropWhile isSpace . reverse . dropWhile isSpace . reverse
+
+mktime :: Integer -> Int -> Int -> Int -> Int -> UTCTime
+mktime y mo d h mi = UTCTime (fromGregorian y mo d) (timeToDiffTime (h, mi))
+
+manyTill' p end = loop
+  where
+    loop = do { _ <- lookAhead end; return [] }
+           <|> do { x <- p; xs <- loop; return (x:xs)}
+
+testParse :: IO ()
+testParse = do
+  contents <- lines <$> readFile "C:/users/jumpei/Documents/home/OrgFiles/notes.org"
+  mapM_ (putStrLn . show) $ rights $ map orgLineParse contents
--- OrgParser/test/Data/String/StripSpec.hs
+++ OrgParser/test/Data/String/StripSpec.hs
@@ -12,33 +12,103 @@ import Data.Time
 main :: IO ()
 main = hspec spec
 
+mktime :: Integer -> Int -> Int -> Int -> Int -> UTCTime
+mktime y mo d h mi = UTCTime (fromGregorian y mo d) (timeToDiffTime (h, mi))
+
 spec :: Spec
 spec = do
   describe "strip" $ do
     it "orgTagsParse" $ do
       parse orgTagsParse "" ":hoge:foo:" `shouldBe` Right ["hoge", "foo"]
-      -- parse orgTagsParse "" ":hoge:foo"  `shouldBe` Left ""
+    it "orgTagsParse2" $ do
+      parse orgTagsParse "" "::" `shouldBe` Right []
     it "orgTimeParse" $ do
       parse orgTimeParse "" "12:00" `shouldBe` Right (12, 0)
     it "orgDateYMDParse" $ do
       parse orgDateYMDParse "" "2024-12-01" `shouldBe` Right (2024, 12, 1)
-    it "orgDateCoreParse" $ do
-      parse orgDateCoreParse "" "2024-12-01 月 12:00" `shouldBe`
-        Right (UTCTime (fromGregorian 2024 12 1) (timeToDiffTime (12, 0)))
     it "orgDateCoreParse2" $ do
-      parse orgDateCoreParse "" "2024-12-01 月" `shouldBe`
-        Right (UTCTime (fromGregorian 2024 12 1) (timeToDiffTime (0, 0)))
+      parse orgDateCoreParse "" "2024-12-01 @" `shouldBe`
+        Right (mktime 2024 12 1 0 0)
+    it "orgDateParse1--active date" $ do
+      parse orgDateParse "" "<2024-12-01 @>" `shouldBe`
+        Right (Active $ mktime 2024 12 1 0 0)
+    it "orgDateParse2--active date with time" $ do
+      parse orgDateParse "" "<2024-01-31 @ 13:58>" `shouldBe`
+        Right (Active $ mktime 2024 1 31 13 58)
+    it "orgDateParse3--inactive date" $ do
+      parse orgDateParse "" "[2025-02-28 @]" `shouldBe`
+        Right (InActive $ mktime 2025 2 28 0 0)
+    it "orgDateParse4--inactive date with time" $ do
+      parse orgDateParse "" "[2025-02-28 @ 23:10]" `shouldBe`
+        Right (InActive $ mktime 2025 2 28 23 10)
+    it "orgDateParse5--scheduled date" $ do
+      parse orgDateParse "" "SCHEDULED: <2025-12-01 @>" `shouldBe`
+        Right (Scheduled $ mktime 2025 12 1 0 0)
+    it "orgDateParse6--scheduled date with time" $ do
+      parse orgDateParse "" "SCHEDULED: <2025-12-01 @ 11:59>" `shouldBe`
+        Right (Scheduled $ mktime 2025 12 1 11 59)
+    it "orgDateParse7--deadline date" $ do
+      parse orgDateParse "" "DEADLINE: <2025-03-01 @>" `shouldBe`
+        Right (Deadline $ mktime 2025 3 1 0 0)
+    it "orgDateParse8--deadline date with time" $ do
+      parse orgDateParse "" "DEADLINE: <2025-03-01 @ 12:01>" `shouldBe`
+        Right (Deadline $ mktime 2025 3 1 12 1)
+    it "orgDateParse9--range" $ do
+      parse orgDateParse "" "<2024-12-01 @> - <2024-12-02 .>" `shouldBe`
+        Right (Range (mktime 2024 12 1 0 0, mktime 2024 12 2 0 0))
+    it "orgDateParse10--range with time" $ do
+      parse orgDateParse "" "<2024-12-01 月 12:00> - <2024-12-02 火 12:00>" `shouldBe`
+        Right (Range (mktime 2024 12 1 12 0, mktime 2024 12 2 12 0))
     it "orgTitleParse" $ do
-      parse orgTitleParse "" "**** hoge" `shouldBe` Right "hoge"
+      parse orgTitleParse "" "**** hoge" `shouldBe`
+        Right (OrgTitle { title = "hoge", level = 4, todo = Nothing, children = [] })
     it "orgTitleParse2" $ do
-      parse orgTitleParse "" "**** [10/10] hoge" `shouldBe` Right "hoge"
+      parse orgTitleParse "" "*** [10/10] hoge" `shouldBe`
+        Right (OrgTitle { title = "hoge", level = 3, todo = Nothing, children = [] })
     it "orgTitleParse3" $ do
-      parse orgTitleParse "" "**** TODO [10/10] hoge" `shouldBe` Right "hoge"
+      parse orgTitleParse "" "**** TODO [10/10] hoge" `shouldBe`
+        Right (OrgTitle { title = "hoge", level = 4, todo = Just "TODO", children = [] })
     it "orgTitleParse4" $ do
-      parse orgTitleParse "" "**** DONE [10/10] hoge" `shouldBe` Right "hoge"
+      parse orgTitleParse "" "**** DONE [10/10] hoge" `shouldBe`
+        Right (OrgTitle { title = "hoge", level = 4, todo = Just "DONE", children = [] })
     it "orgTitleParse5" $ do
-      parse orgTitleParse "" "*** DONE [10/10] ho ge" `shouldBe` Right "ho ge"
-    -- it "removes leading and trailing whitespace" $ do
-    --   strip "\t  foo bar\n" `shouldBe` "foo bar"
-    -- it "is idempotent" $ property $
-    --   \str -> strip str === strip (strip str)
+      parse orgTitleParse "" "*** DONE [10/10] ho ge" `shouldBe`
+        Right (OrgTitle { title = "ho ge", level = 3, todo = Just "DONE", children = [] })
+    it "orgTitleParse6" $ do
+      parse orgTitleParse "" "*** DONE [10/10] ho ge  ::" `shouldBe`
+        Right (OrgTitle { title = "ho ge  ", level = 3, todo = Just "DONE", children = [] })
+    it "orgTitleParse7" $ do
+      parse orgTitleParse "" "*** DONE [10/10] ho ge  :hoge:" `shouldBe`
+        Right (OrgTitle { title = "ho ge  ", level = 3, todo = Just "DONE", children = [] })
+    it "orgTitleParse8" $ do
+      parse orgTitleParse "" "*** DONE [10/10] ho ge  <2024-12-01 @ 12:00>" `shouldBe`
+        Right (OrgTitle { title = "ho ge  ", level = 3, todo = Just "DONE", children = [] })
+    it "orgTitleParse9" $ do
+      parse orgTitleParse "" "*** DONE [10/10] ho ge  <2024-12-01 @ 12:00> :hoge:" `shouldBe`
+        Right (OrgTitle { title = "ho ge  ", level = 3, todo = Just "DONE", children = [] })
+    it "orgTitleParse10--unit test" $ do
+      let title  = "*** DONE [10/10] ho ge  <2024-12-01 @ 12:00> :hoge:"
+          parser = (,,) <$> (orgTitleParse <* many space)
+                        <*> (orgDateParse <* many space)
+                        <*> orgTagsParse
+      parse parser "" title `shouldBe`
+        Right (OrgTitle { title = "ho ge  ", level = 3, todo = Just "DONE", children = [] }
+              , Active (mktime 2024 12 1 12 0)
+              , ["hoge"])
+    it "orgPropertyParse" $ do
+      parse orgPropertyParse "" ":PROPERTIES:" `shouldBe` Right PropBegin
+    it "orgPropertyParse2" $ do
+      parse orgPropertyParse "" ":END:" `shouldBe` Right PropEnd
+    it "orgPropertyParse3" $ do
+      parse orgPropertyParse "" ":LOCATION:  hoge" `shouldBe` Right (PropLocation "hoge")
+    it "orgPropertyParse4" $ do
+      parse orgPropertyParse "" ":JOINER: ho ge" `shouldBe` Right (PropJoiner "ho ge")
+    it "orgPropertyParse5" $ do
+      parse orgPropertyParse "" ":CUSTOM_ID: 2 04" `shouldBe`
+        Right (PropOther ("CUSTOM_ID", "2 04"))
+    it "orgLinkParse1" $ do
+      parse orgLinkParse "" "[[http://www.google.co.jp]]" `shouldBe`
+        Right (OrgLink "http://www.google.co.jp" Nothing)
+    it "orgLinkParse2" $ do
+      parse orgLinkParse "" "[[http://www.google.co.jp][Google]]" `shouldBe`
+        Right (OrgLink "http://www.google.co.jp" (Just "Google"))
