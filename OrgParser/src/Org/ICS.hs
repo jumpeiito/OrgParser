7f15d70765f53973ddc628a072e1851119320a91
jumpei <junnpit@gmail.com>
2025-03-18 18:02:53 +0900

    planning network response

--- OrgParser/src/Org/ICS.hs
+++ OrgParser/src/Org/ICS.hs
@@ -1,14 +1,25 @@
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds         #-}
 module Org.ICS
   (
     VEVENT (..)
+  , VCalendar (..)
   , veventList
+  , nodeToVCalendar
+  , vcalendarToString
   )
 where
 
+import  Data.Maybe              (fromMaybe)
+import  Data.List               (intercalate)
 import  Data.Time
 import  Data.Time.Format
+import  Network.HTTP.Req
 import  Org.Parse
 import  Org.Node
+import  Text.Hamlet
+import  Text.Blaze.Html.Renderer.String
 
 jpTimeLocale :: TimeLocale
 jpTimeLocale =
@@ -28,10 +39,35 @@ data VEVENT = VEVENT { vstarttime :: UTCTime
                      , vlocation  :: Maybe String
                      , vdesc      :: String
                      }
+  deriving (Eq)
+
+data VCalendar = VCalendar [VEVENT]
   deriving (Show, Eq)
 
+testV = VEVENT (mktime 2025 3 18 12 0) Nothing "" Nothing ""
+
+instance Show VEVENT where
+  show v =
+    renderHtml [shamlet|
+                       BEGIN:VEVENT
+                       DTSTART:#{vs}
+                       #{dtend}
+                       LOCATION:#{vl}
+                       DESCRIPTION:#{vd}
+                       SUMMARY:#{vt}
+                       END:VEVENT
+                       |]
+    where
+      vs = utcToICS $ vstarttime v
+      vl = mempty `fromMaybe` vlocation v
+      vd = vdesc v
+      vt = vtitle v
+      dtend = mempty `fromMaybe` ((("DTEND:" ++) . utcToICS) <$> vendtime v)
+
 utcToICS :: UTCTime -> String
-utcToICS = formatTime jpTimeLocale "%Y%m%dT%H%M%S"
+utcToICS utc
+  | utctDayTime utc == 0 = formatTime jpTimeLocale "%Y%m%d" utc
+  | otherwise = formatTime jpTimeLocale "%Y%m%dT%H%M%S" utc
 
 orgNodetoVEVENT :: String -> OrgElement -> OrgNode -> VEVENT
 orgNodetoVEVENT path timestamp node =
@@ -39,7 +75,7 @@ orgNodetoVEVENT path timestamp node =
          , vendtime   = end timestamp
          , vtitle     = node ~~> title
          , vlocation  = location node
-         , vdesc      = path <> "\n" <> description node
+         , vdesc      = path <> description node
          }
 
 veventList :: OrgNode -> [VEVENT]
@@ -49,3 +85,37 @@ veventList node =
   where
     collecter = collectNodeList filf (Just node)
     filf n    = (hasAliveTime n) && (notTODO n)
+
+nodeToVCalendar :: OrgNode -> VCalendar
+nodeToVCalendar = VCalendar . veventList
+
+vcalendarToString :: VCalendar -> String
+vcalendarToString (VCalendar vevs) =
+  renderHtml [shamlet|
+                     BEGIN:VCALENDAR
+                     VERSION:2.0
+                     PRODID:-//hacksw/handcal//NONSGML v1.0//EN
+                     #{vevents}
+                     END:VCALENDAR
+                     |]
+  where
+    vevents = intercalate "\n" $ map show vevs
+
+postSample :: IO ()
+postSample = do
+  runReq defaultHttpConfig $ do
+    res <- req
+      POST
+      url
+      NoReqBody
+      ignoreResponse
+      mempty
+    return ()
+    -- liftIO $ print (responseBody res :: Value)
+      where
+        url :: Url 'Https
+        url = https "googleapis.com" /: "calendar" /: "v3" /: "calendars" /: "junnpit@gmail.com" /: "events" /: "import"
+        -- https://www.googleapis.com/calendar/v3/calendars/calendarId/events/import
+        -- params :: FormUrlEncodedParam
+        -- params =    "key1" =: ("value1" :: Text)
+        --             <> "key2" =: (2 :: Int)
--- OrgParser/src/Org/Test.hs
+++ OrgParser/src/Org/Test.hs
@@ -18,15 +18,22 @@ testData :: IO [OrgElement]
 testData = do
   -- contents <- lines <$> readFile "C:/users/jumpei/Documents/home/OrgFiles/notes.org"
   -- contents <- lines <$> readFile "c:/Users/kkr0133/Documents/OrgParser/OrgParser/src/Data/String/test.org"
-  -- contents <- lines <$> readFile "e:/Dropbox/notes.org"
-  contents <- lines <$> readFile "C:/users/jumpei/Documents/home/OrgFiles/notes.org"
+  contents <- lines <$> readFile "e:/Dropbox/notes.org"
+  -- contents <- lines <$> readFile "C:/users/jumpei/Documents/home/OrgFiles/notes.org"
   return $ concatMap ((mempty `fromRight`) . orgLineParse) contents
 
 testPrint :: IO ()
 testPrint = do
   dx:dxs <- testData
   let Just node = foldl (flip addNode) (makeOrgNode dx) dxs
-  forM_ (veventList node) print
+  putStrLn $ vcalendarToString $ nodeToVCalendar node
     -- putStrLn $ path ++ ":" ++ (mempty `fromMaybe` showTitle e) ++ ", " ++ (show (length (timestamps e)))
 
   -- print node
+testWrite :: IO ()
+testWrite = do
+  let icsfile = "./org.ics"
+  dx:dxs <- testData
+  let Just node = foldl (flip addNode) (makeOrgNode dx) dxs
+  let ics = vcalendarToString $ nodeToVCalendar node
+  writeFile icsfile ics
