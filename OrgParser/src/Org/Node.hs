6ad4439ba24ae5d553d2a0084b46f5ac26315714
jumpei <junnpit@gmail.com>
2025-03-19 19:41:40 +0900

    Node.hs: instance.

--- OrgParser/src/Org/Node.hs
+++ OrgParser/src/Org/Node.hs
@@ -1,112 +1,144 @@
+{-# LANGUAGE InstanceSigs, DeriveFunctor, DeriveFoldable, FlexibleContexts, ScopedTypeVariables #-}
 module Org.Node
   (
-    timestamps
-  , hasChildrenAliveTime
-  , hasAliveTime
-  , notTODO
-  , notChildrenTODO
-  , nodeToList
-  , collectNodeList
-  , addNode
-  , showTitle
-  , makeOrgNode
-  , location
-  , description
-  , (~~>)
-  , OrgNode (..)
+  --   timestamps
+  -- , hasChildrenAliveTime
+  -- , hasAliveTime
+  -- , notTODO
+  -- , notChildrenTODO
+  -- , nodeToList
+  -- , collectNodeList
+  -- , addNode
+  -- , showTitle
+  -- , makeOrgNode
+  -- , location
+  -- , description
+  -- , (~~>)
+  -- , OrgNode (..)
   )
 where
 
 import Org.Parse
-import Control.Monad
 import Data.Maybe
-import Data.Either
 
-type NextNode  = Maybe OrgNode
-type ChildNode = Maybe OrgNode
-data OrgNode   = OrgNode OrgElement NextNode ChildNode
-  deriving (Show, Eq)
+data OrgNode a = OrgNode a (Maybe (OrgNode a)) (Maybe (OrgNode a))
+  deriving (Show, Eq, Functor, Foldable)
 
-makeOrgNode :: OrgElement -> Maybe OrgNode
+type OrgNodeElement = OrgNode OrgElement
+
+testTree = OrgNode 20 (Just $ OrgNode 200 Nothing Nothing)
+                      (Just $ OrgNode 300 (Just $ OrgNode 12 Nothing Nothing)
+                                          (Just $ OrgNode 100 Nothing Nothing))
+instance Traversable OrgNode where
+  traverse f (OrgNode el (Just n) (Just c)) =
+    OrgNode (f el) (Just $ traverse f n) (Just $ traverse f c)
+  traverse f (OrgNode el (Just n) Nothing) =
+    OrgNode (f el) (Just $ traverse f n) Nothing
+  traverse f (OrgNode el Nothing (Just c)) =
+    OrgNode (f el) Nothing (Just $ traverse f c)
+  traverse f (OrgNode el Nothing Nothing) =
+    OrgNode (f el) Nothing Nothing
+-- instance Applicative OrgNode where
+--   pure a = OrgNode a Nothing Nothing
+
+--   OrgNode f f1 f2 <*> OrgNode el n c =
+--     OrgNode (f el) (cus f1 n) (cus f2 c)
+--     where
+--       core p1 p2 = do
+--         f <- 
+--       cus Nothing _ = Nothing
+--       cus _ Nothing = Nothing
+--       cus (Just (OrgNode elf1 n1 c1)) (Just (OrgNode elf2 n2 c2)) =
+--         Just (OrgNode (elf1 elf2) (n1 <*> n2) (c1 <*> c2))
+      -- cus (Just (OrgNode elf n' c')) node =
+      --   pure <$> (elf <$> (elements <$> node))
+        -- toNode <$> (elf <$> (elements <$> node))
+        -- toNode n' c' <$> (elf <$> (elements <$> node))
+
+-- testFold = foldMap (*) testTree
+elements :: OrgNode a -> a
+elements (OrgNode el _ _) = el
+
+makeOrgNode :: OrgElement -> Maybe OrgNodeElement
 makeOrgNode el = Just $ OrgNode el Nothing Nothing
 
-showTitle :: OrgNode -> Maybe String
+showTitle :: OrgNodeElement -> Maybe String
 showTitle (OrgNode el _ _)
   | isTitle el = Just $ title el
   | otherwise  = Nothing
 
-(~~>) :: OrgNode -> (OrgElement -> a) -> a
+(~~>) :: OrgNodeElement -> (OrgElement -> a) -> a
 (~~>) (OrgNode el _ _) f = f el
 
-timestamps :: OrgNode -> [OrgElement]
+timestamps :: OrgNodeElement -> [OrgElement]
 timestamps node = filter isTimeStamp $ node ~~> children
 
-hasAliveTime :: OrgNode -> Bool
+hasAliveTime :: OrgNodeElement -> Bool
 hasAliveTime = not . null. filter notCloseOrInActive . timestamps
   where
     notCloseOrInActive timestamp =
       ((datetype timestamp) /= Closed) && ((active timestamp) /= False)
 
-hasChildrenAliveTime :: OrgNode -> Bool
+hasChildrenAliveTime :: OrgNodeElement -> Bool
 hasChildrenAliveTime o@(OrgNode _ _ Nothing)      = hasAliveTime o
 hasChildrenAliveTime o@(OrgNode _ _ (Just child)) =
   (hasAliveTime o) || (hasAliveTime child)
 
-notTODO :: OrgNode -> Bool
+notTODO :: OrgNodeElement -> Bool
 notTODO = not . isJust . (~~> todo)
 
-notChildrenTODO :: OrgNode -> Bool
+notChildrenTODO :: OrgNodeElement -> Bool
 notChildrenTODO (OrgNode name _ Nothing) = not $ isJust $ todo name
-notChildrenTODO o@(OrgNode name _ (Just child)) = notTODO o || notChildrenTODO child
+notChildrenTODO o@(OrgNode _ _ (Just child)) = notTODO o || notChildrenTODO child
 
-location :: OrgNode -> Maybe String
+location :: OrgNodeElement -> Maybe String
 location node = loop $ node ~~> children
   where
     loop [] = Nothing
-    loop ((OrgProperty ("LOCATION", val)):xs) = Just val
-    loop (x:xs) = loop xs
+    loop ((OrgProperty ("LOCATION", val)):_) = Just val
+    loop (_:xs) = loop xs
 
-description :: OrgNode -> String
+description :: OrgNodeElement -> String
 description node = loop mempty $ node ~~> children
   where
     loop s [] = s
     loop s ((OrgOther o):xs) = loop (s <> o) xs
     loop s (_:xs) = loop s xs
 
-addNode :: OrgElement -> Maybe OrgNode -> Maybe OrgNode
-addNode el Nothing = makeOrgNode el
-addNode el (Just (OrgNode name next child)) =
-  case (isTitle el, isJust next, isJust child, el `orgTitleOrder` name) of
-    (True, _, _, Just EQ) ->
-      Just $ OrgNode name (addNode el next) child
-    (True, True, _, Just GT) ->
-      Just $ OrgNode name (addNode el next) child
-    (True, False, _, Just GT) ->
-      Just $ OrgNode name next (addNode el child)
-    (True, _, _, Just LT) ->
-      error "must not happen"
-    (True, _, _, Nothing) -> Nothing
-    (False, True, _, _) ->
-      Just $ OrgNode name (addNode el next) child
-    (False, False, True, _) ->
-      Just $ OrgNode name next (addNode el child)
-    (False, False, False, _) ->
-      let c = children name ++ [el] in
-      Just $ OrgNode (name { children = c }) next child
+-- addNode :: OrgElement -> Maybe OrgNode -> Maybe OrgNode
+-- addNode el Nothing = makeOrgNode el
+-- addNode el (Just (OrgNode name next child))
+--   | isTitle name =
+--     case (isTitle el, isJust next, isJust child, el `orgTitleOrder` name) of
+--       (True, _, _, Just EQ) ->
+--         Just $ OrgNode name (addNode el next) child
+--       (True, True, _, Just GT) ->
+--         Just $ OrgNode name (addNode el next) child
+--       (True, False, _, Just GT) ->
+--         Just $ OrgNode name next (addNode el child)
+--       (True, _, _, Just LT) ->
+--         error "must not happen"
+--       (True, _, _, Nothing) -> Nothing
+--       (False, True, _, _) ->
+--         Just $ OrgNode name (addNode el next) child
+--       (False, False, True, _) ->
+--         Just $ OrgNode name next (addNode el child)
+--       (False, False, False, _) ->
+--         let c = children name ++ [el] in
+--           Just $ OrgNode (name { children = c }) next child
+-- addNode _ (Just _) = Nothing
 
-nodeToList :: Maybe OrgNode -> [(String, OrgNode)]
-nodeToList = collectNodeList (const True)
+node2List :: OrgNodeElement -> [(String, OrgElement)]
+node2List = collectNodeList (const True)
 
-collectNodeList :: (OrgNode -> Bool) -> Maybe OrgNode -> [(String, OrgNode)]
-collectNodeList _ Nothing = []
-collectNodeList f node = loop "" [] node
+collectNodeList ::
+  (OrgElement -> Bool) -> OrgNodeElement -> [(String, OrgElement)]
+collectNodeList f = fst . foldr core ([], "")
   where
-    loop _ ret Nothing = ret
-    loop path ret (Just o@(OrgNode name next child)) =
-      let newpath = path ++ "/" ++ title name
-          attach  = if (f o) then [(newpath, o)] else [] in
-        attach ++ (loop newpath ret child) ++ (loop path ret next)
-
+    core el seed@(ret, path)
+      | f el = let newpath = path ++ "/" ++ title el in
+                 ((newpath, el):ret, newpath)
+      | otherwise = seed
 
 isTitle :: OrgElement -> Bool
 isTitle (OrgTitle _ _ _ _) = True
